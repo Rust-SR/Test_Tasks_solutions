a3a83917bb3f6d84f69d3a2b70815d6ed8f65ec7
unknown <rozfire44@gmail.com>
2022-03-12 16:54:35 +0500

    added sql and linux test task

---
+++ SQL and Linux command line/TZ_sql_airplanes_sql.sql
@@ -0,0 +1,59 @@
+-- Создаем таблицу airplanes
+create table airplane (
+	airplane_id serial primary key,
+	model varchar(30) unique not null
+);
+
+-- Вносим модели самолетов
+insert into airplane(model)
+values ('Boeing'), ('Сухой'), ('Airbus');
+-- Создаем таблицу pilot
+create table pilot (
+	pilot_id serial primary key,
+	first_name varchar(30),
+	last_name varchar(30)	
+);
+
+-- Вносим имена пилотов
+insert into pilot(first_name, last_name)
+values ('Сергей', 'Шарапов'); 
+insert into pilot(first_name, last_name)
+values ('Саня', 'Сыч');
+insert into pilot(first_name, last_name)
+values ('Маша', 'Сыч');
+insert into pilot(first_name, last_name)
+values ('Дима', 'Валиев');
+insert into pilot(first_name, last_name)
+values ('Ира', 'Смирнова');
+
+-- Создаем таблицу airplane_pilot и устанавливаем связь между всеми таблицами
+create table airplane_pilot (
+	id serial primary key,
+	pilot_id int not null,
+	airplane_id int not null,
+	flight_date varchar(30) not null,
+	foreign key (pilot_id)
+		references pilot(pilot_id),
+	foreign key (airplane_id)
+		references airplane(airplane_id)
+);
+
+-- Вносим данные в таблицу согласно оригинальному ТЗ
+insert into airplane_pilot(pilot_id, airplane_id, flight_date)
+values (1, 1, '2022-02-28'),
+(2, 1, '2022-02-28'),
+(1, 1, '2022-02-28'),
+(3, 1, '2022-02-28'),
+(4, 2, '2022-02-28'),
+(5, 2, '2022-02-28'),
+(4, 2, '2022-02-28'),
+(1, 1, '2022-02-28');
+
+-- Выводим количество полетов на Boeing каждого пилота
+select count(*) as Boeing_Flights, pilot.first_name, pilot.last_name
+from airplane_pilot
+join pilot on airplane_pilot.pilot_id=pilot.pilot_id
+join airplane on airplane_pilot.airplane_id=airplane.airplane_id
+where airplane.airplane_id = 1
+group by pilot.first_name, pilot.last_name;
+
